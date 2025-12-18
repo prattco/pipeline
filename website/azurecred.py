40ba182a262baf458d56c8e2456102fb645fb2b1
prattco <danny.yun@prattco.com>
2025-12-18 09:27:18 -0600

    clean up

--- website/azurecred.py
+++ website/azurecred.py
@@ -3,6 +3,5 @@
 AZDBSERVER = 'pipeline-server.database.windows.net'
 AZDBNAME = 'pipeline-database'
 AZDBUSER = 'pipeline-server-admin'
-AZDBPW = '$zFSJnl44aWSxamk'
+AZDBPW = '$RRk3P9RvazIdv$x'
 AZDBDRIVER = 'ODBC Driver 17 for SQL Server'
-
